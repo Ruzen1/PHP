88146e8959bfa5a3fe813aa35926981354ff16b6
Ruzen1 <rujen-shakya@hotmail.com>
2019-08-06 10:20:03 +0900

    lala

--- test.php
+++ test.php
@@ -1,9 +1,21 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <body>
 <?php
-$x=10.365;
-var_dump($x);
-?>
-</body>
-</html>
+    $cars=array
+    (
+        array("Volvo",22,18),
+        array("BMW",15,13),
+        array("Saab",5,2),
+        array("Land Rover",17,15),   
+    );
+
+    echo $cars[0][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
+    echo $cars[1][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
+    echo $cars[2][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
+    echo $cars[3][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
+    ?>
+
+    </body>
+    </html>
+    
