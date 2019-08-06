b753d6448c33b7e364e97a67155d1f4efff45375
Ruzen1 <rujen-shakya@hotmail.com>
2019-08-06 10:54:54 +0900

    sss

--- test.php
+++ test.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-$d=strtotime("tomorrow");
-echo date("Y-m-d h:i:sa",$d)."<br>";
+$startdate=strtotime("Saturday");
+$enddate=strtotime("+6 weeks",$startdate);
 
-$d=strtotime("next Saturday");
-echo date("Y-m-d h:i:sa",$d)."<br>";
-
-$d=strtotime("+3 Months");
-echo date("Y-m-d h:i:sa",$d)."<br>";
-
+while ($startdate < $enddate){
+    echo date("M d", $startdate) . "<br>";
+    $startdate = strtotime("+1 week",$startdate);
+}
 ?>
+</body>
+</html>
