1c8795c1d5d986d26c7d5f6627c8147ea0b9285d
Ruzen1 <rujen-shakya@hotmail.com>
2019-08-06 10:51:06 +0900

    dd

--- test.php
+++ test.php
@@ -3,8 +3,13 @@
 <body>
 
 <?php
-$d=strtotime("10:30pm  April 15 2014 ");
-echo "Created date is " . date ("Y-m-d h:i:sa",$d);
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa",$d)."<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa",$d)."<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa",$d)."<br>";
+
 ?>
-</body>
-</html>
