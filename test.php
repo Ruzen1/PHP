4567870825d5456c7b0c34e69d6567e23c1fe4c0
Ruzen1 <rujen-shakya@hotmail.com>
2019-08-06 10:31:45 +0900

    date

--- test.php
+++ test.php
@@ -1,21 +1,11 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 <body>
 <?php
-    $cars=array
-    (
-        array("Volvo",22,18),
-        array("BMW",15,13),
-        array("Saab",5,2),
-        array("Land Rover",17,15),   
-    );
-
-    echo $cars[0][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
-    echo $cars[1][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
-    echo $cars[2][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
-    echo $cars[3][0].": In stock: ".$cars[0][1].",sold: ".$cars[0][2].".<br>";
-    ?>
-
-    </body>
-    </html>
-    
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("y-M-D-L") . "<br>";
+echo "Today is " . date("1");
+?>
+</body>
+</html>
